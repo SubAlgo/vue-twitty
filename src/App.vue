5a4f7c22842e396b2a3dcf5c529c0c7616299291
korn <kornchaval@gmail.com>
2017-07-16 07:50:40 +0700

    Create <router-link> user.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    555
+    <tr>
+      <td><router-link to='/'>Home</router-link></td>
+      <td><router-link to='/profile'>Profile</router-link></td>
+      <td><router-link to="/user/1">User 1</router-link></td>
+      <td><router-link to='/singin'>Singin</router-link></td>
+    </tr>
+
     <router-view></router-view>
   </div>
 </template>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>User</div>
+  <div>
+    User {{ id }} <br>
+    <router-link :to="`/user/${id - 1} `">Prev</router-link>
+    <router-link :to="`/user/${id + 1} `">Next</router-link>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      id: 0
+    }
+  },
+  created () {
+    this.id = +this.$route.params.id
+  },
+  watch: {
+    $route () {
+      this.id = +this.$route.params.id
+    }
+  }
 }
 </script>
-
-<style lang="css">
-</style>
